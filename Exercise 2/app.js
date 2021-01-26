325217b8450ca74b43e6b2e942b36715f8e00ce7
Kishan Sheth <kishansheth21@gmail.com>
2021-01-26 16:45:25 +0530

    Second Exercise

---
+++ Exercise 2/app.js
@@ -0,0 +1,136 @@
+//On Page Load Hide the modify section
+$(document).ready(function () {
+  $("#modify").hide();
+});
+
+//array for all the selected fileds
+let selectedArray = [];
+
+//function for adding new rows to the tables
+const addRow = (event) => {
+  event.preventDefault();
+  const firstName = $("#data1").val();
+  const lastName = $("#data2").val();
+  const color = $("#color").val();
+  const txtSize = $("#txtSize").val();
+  //Unique id for all rows
+  const id = Date.now();
+  const string = `<tr id=${id}>
+                      <td><input type="checkbox" class="form-check-input" onchange="selectDeselect(${id})" /></td>
+                      <td style="background-color:${color}; font-size:${txtSize}">${firstName}</td>
+                      <td style="background-color:${color}; font-size:${txtSize}">${lastName}</td>
+                      <td><button class="btn btn-success" onclick="editRow(${id})">EDIT</button></td>
+                      <td><button class="btn btn-danger" onclick="deleteRow(${id})">DELETE</button></td>
+                    </tr>`;
+  //Appending the new row to the end of the tablebody
+  $("table > tbody").append(string).hide().fadeIn(1000);
+  //Form Reset after the values have been added
+  $("#addForm")[0].reset();
+};
+
+// Function to delete the Rows
+const deleteRow = (id) => {
+  $(`#${id}`).fadeOut(500, function () {
+    $(this).remove();
+  });
+};
+
+//Function for editing the row
+const editRow = (id) => {
+  const firstName = $(`#${id} td:eq(1)`).html();
+  const lastName = $(`#${id} td:eq(2)`).html();
+  $("#addBtn").html("Update").attr("onclick", `updateRow(${id})`);
+  $("#data1").val(firstName);
+  $("#data2").val(lastName);
+  $("#addBtn").toggleClass("btn-success");
+  $("#addBtn").toggleClass("btn-primary");
+  $("#color").prop("disabled", true);
+  $("#txtSize").prop("disabled", true);
+};
+
+//Function for updating the row
+const updateRow = (id) => {
+  const firstName = $("#data1").val();
+  const lastName = $("#data2").val();
+  $(`#${id} td:eq(1)`).html(firstName);
+  $(`#${id} td:eq(2)`).html(lastName);
+  $("#addBtn").html("Add").attr("onclick", `addRow()`);
+  $("#addBtn").toggleClass("btn-success");
+  $("#addBtn").toggleClass("btn-primary");
+  $("#color").prop("disabled", false);
+  $("#txtSize").prop("disabled", false);
+  $("#addForm")[0].reset();
+};
+
+//Function to select all whenever select all is checked
+const selectAll = () => {
+  const state = $("#all").is(":checked");
+  selectedArray = [];
+  $("tr").each(function () {
+    $(this).children()[0].children[0].checked = state;
+    if (state) {
+      selectedArray.push($(this).attr("id"));
+    }
+  });
+  formString();
+};
+
+//Function to insert into selectedArray whenever anything is selected also pop whenever deselcted
+const selectDeselect = (id) => {
+  const state = $(`#${id}`).children()[0].children[0].checked;
+  if (state) {
+    selectedArray.push(id);
+  } else {
+    const index = selectedArray.indexOf(id);
+    selectedArray.splice(index, 1);
+  }
+  formString();
+};
+
+//Function for deleting the selected rows
+const deleteSelected = () => {
+  selectedArray.forEach((id) => {
+    $(`#${id}`).fadeOut(500, function () {
+      $(this).remove();
+    });
+  });
+  //Emptying the selected Array after all selected values are deleted
+  selectedArray = [];
+  formString();
+};
+
+//Function for shoing the string of how many is selected
+const formString = () => {
+  $("#totalSelected").html(selectedArray.length);
+  //calling the modify function whenever anything is selected
+  if (selectedArray.length > 0) showModify(true);
+  else showModify(false);
+};
+
+//Function to change color and font
+const applyChange = (event) => {
+  event.preventDefault();
+  const color = $("#changeColor").val();
+  const text = $("#changeTextSize").val();
+  //For all selected id's in the selected array change the text size and background color
+  selectedArray.forEach((id) => {
+    $(`#${id}`).children()[1].style.backgroundColor = color;
+    $(`#${id}`).children()[2].style.backgroundColor = color;
+    $(`#${id}`).children()[1].style.fontSize = text;
+    $(`#${id}`).children()[2].style.fontSize = text;
+  });
+  //After the values have been changed reset the form
+  $("#modifyForm")[0].reset();
+};
+
+//Function to show and hide the modify section
+const showModify = (choice) => {
+  //if choice is true then show the section
+  if (choice) {
+    $("#modify").show();
+  }
+  // if choice is false then hide the section
+  if (!choice) {
+    $("#modify").hide();
+  }
+};
